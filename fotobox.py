c5f35fac68e83cf94c89d105fb7a6cd6e032f8af
Paul Nehlich <in@paul-nehlich.de>
2023-04-18 08:10:18 +0200

    .

--- fotobox.py
+++ fotobox.py
@@ -179,10 +179,11 @@ class Ui_Form_mod(object):
     self.lastPhoto = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jpg"
     if not fotoboxCfg['nopi']:
       #self.camera.resolution = (fotoboxCfg['cam-c-width'], fotoboxCfg['cam-c-height'])
-      self.lastPhoto = self.camera.capture(gp.GP_CAPTURE_IMAGE)
-      target = os.path.join(self.temp, file_path.name)
+      file_path = self.camera.capture(gp.GP_CAPTURE_IMAGE)
+      self.lastPhoto = file_path.name
+      target = os.path.join(self.temp, self.lastPhoto)
       self.camera_file = self.camera.file_get(
-          file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+          file_path.folder, self.lastPhoto, gp.GP_FILE_TYPE_NORMAL)
       self.camera_file.save(target)
       self.camera.exit()
       subprocess.call(['xdg-open', self.temp+self.lastPhoto])
